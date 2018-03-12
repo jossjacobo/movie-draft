988cacf087d08fcb7744c199c9cb1f80e1999f30
Jossay Jacobo <jossjacobo@gmail.com>
2018-03-11 22:01:19 -0400

    Removed api from this project

--- src/api/config/db.js
+++
@@ -1,4 +0,0 @@
-// https://mlab.com/databases/movie-draft-db
-module.exports = {
-  url: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds251598.mlab.com:51598/${process.env.DB_NAME}`
-}
--- src/api/routes/index.js
+++
@@ -1,6 +0,0 @@
-const routes = require('./routes');
-
-module.exports = function (app, db) {
-  routes(app, db);
-  // other routes can go here
-};
